a143d19caf8611b16a22ab7f40178034d0393594
Emma Wilby <emma@pwilby.co.uk>
2020-12-09 15:25:02 +0000

    completed rollingDice code along

---
+++ CodeAlongs/rollingDice/rollingDice.js
@@ -0,0 +1,11 @@
+var numSides = prompt("How many sides does the die have?");
+
+var numRolls = prompt("How many dice rolls?");
+
+function rollDice(numSides) {
+  return Math.floor(Math.random() * numSides) + 1;
+}
+
+for (var i = 0; i < numRolls; i++) {
+  console.log(rollDice(numSides));
+}
